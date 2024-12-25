4d41fcca594a9029f9d7360a620a628019fa7790
YousefFawzy2222 <117592164+YousefFawzy2222@users.noreply.github.com>
2024-12-25 14:41:39 +0200

    Confirm
    Confirm

---
+++ RESTApi/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly AdventureWorksDbContext _context;
+
+    public ProductsController(AdventureWorksDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        return product != null ? Ok(product) : NotFound();
+    }
+}
